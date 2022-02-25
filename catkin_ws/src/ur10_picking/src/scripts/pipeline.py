d518ba8beb80ccf18f1f081aa993cb4fb481464a
James Ward <vz21101@bristol.ac.uk>
2022-02-25 12:33:35 +0000

    fixed pipeline name, added incremental movement and better startup procedures (still in progress)

---
+++ catkin_ws/src/ur10_picking/src/scripts/pipeline.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Pose
+from ur10_picking.msg import poseMessage 
+
+class PoseTalker():
+
+    def __init__(self, publisher_name):
+
+        self.pub = rospy.Publisher(publisher_name, poseMessage, queue_size=10)
+
+    def send(self, pose, incremental=False):
+        pose_msg = poseMessage()
+        pose_msg.pose = pose
+        pose_msg.incremental = incremental
+        self.pub.publish(pose_msg)
+
+
+def print_callback(arg):
+    print("----------------")
+    print(rospy.get_time())
+    print(arg)
+
+
+class Pipeline():
+
+    def __init__(self):
+    
+        rospy.init_node("pipeline", anonymous=False)
+        self.rate = rospy.Rate(10)
+        self.pose_talker = PoseTalker('/pipeline/next_cartesian_pose')
+        rospy.Subscriber('/moveit_interface/cartesian_pose_feedback', poseMessage, print_callback)
+    
+    def pose_start(self):
+
+        start_pose = Pose()
+        start_pose.position.x = -0.1
+        start_pose.position.y = 0.5
+        start_pose.position.z = 0.3
+        start_pose.orientation.x = 0
+        start_pose.orientation.y = 0
+        start_pose.orientation.z = 0.707
+        start_pose.orientation.w = 0.707
+    
+        self.pose_talker.send(start_pose)
+
+        rospy.sleep(5.0)
+ 
+
+if __name__ == "__main__":
+
+    pipeline = Pipeline()
+    rospy.sleep(5.0) # To allow robot to home before sending start pose
+    pipeline.pose_start()
+
+    pose_adjustment = Pose()
+    # pose_adjustment.position.x = 0.01
+
+    while not rospy.is_shutdown():
+        # pipeline.pose_talker.send(pose_adjustment, incremental=True)        
+        pipeline.rate.sleep()   
+
--- catkin_ws/src/ur10_picking/src/scripts/ur10_motion_interface.py
+++ catkin_ws/src/ur10_picking/src/scripts/ur10_motion_interface.py
@@ -3,16 +3,19 @@ import rospy
 import sys
 import moveit_commander
 from geometry_msgs.msg import Pose
-
+from ur10_picking.msg import poseMessage
 
 class PoseTalker():
 
     def __init__(self, publisher_name):
 
-        self.pub = rospy.Publisher(publisher_name, Pose, queue_size=10)
+        self.pub = rospy.Publisher(publisher_name, poseMessage, queue_size=10)
 
-    def send(self, pose):
-        self.pub.publish(pose)
+    def send(self, pose, incremental=False):
+        pose_msg = poseMessage()
+        pose_msg.pose = pose
+        pose_msg.incremental = incremental
+        self.pub.publish(pose_msg)
 
 
 class MoveitInterface():
@@ -26,15 +29,30 @@ class MoveitInterface():
         self.move_group = moveit_commander.MoveGroupCommander('manipulator')
         
         self.pose_talker = PoseTalker('/moveit_interface/cartesian_pose_feedback')
-        self.sub = rospy.Subscriber('/controller/next_cartesian_pose', Pose, self.move_to_pose)
+        self.sub = rospy.Subscriber('/pipeline/next_cartesian_pose', poseMessage, self.move_to_pose)
 
-    def move_to_pose(self, pose):
+        self.move_group.set_joint_value_target([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.move_group.go(wait=True)
+        self.move_group.stop()
+        self.move_group.clear_pose_targets()
+
+    def move_to_pose(self, pose_message):
+
+        if pose_message.incremental:
+            pose = self.move_group.get_current_pose()
+            pose.x += pose_message.pose.position.x
+            pose.y += pose_message.pose.position.y
+            pose.z += pose_message.pose.position.z
+        else:
+            pose = pose_message.pose
+
         self.move_group.set_pose_target(pose)
         self.move_group.go(wait=True)
         self.move_group.stop()
         self.move_group.clear_pose_targets()
 
     def feedback(self):
+    
         pose = self.move_group.get_current_pose().pose
         self.pose_talker.send(pose)
 
