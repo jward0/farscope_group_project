273980f182468f8e0e0593ee1ad1e1f603e99fa0
Gjosse Zijlstra <gjossep@gmail.com>
2022-03-25 12:47:49 +0000

    Updated basic code with flip

--- catkin_ws/src/ur10_picking/src/scripts/playground.py
+++ catkin_ws/src/ur10_picking/src/scripts/playground.py
@@ -4,91 +4,6 @@ import numpy as np
 import pyrealsense2 as rs
 import matplotlib.pyplot as plt
 
-from sklearn.cluster import KMeans
-
-from treelib import Node, Tree
-
-class Quad(object):
-    def __init__(self, indexX, indexY, data):
-        self.indexX = indexX
-        self.indexY = indexY
-        self.data = data
-        self.colour = None
-
-def check_homogeneity(image):
-    max_value = np.amax(image)
-    min_value = np.amin(image)
-    #print("max_dif: %f" % (max_value-min_value))
-    if max_value-min_value >= 1:
-        return False
-    else:
-        return True 
-
-def process_nodes(tree):
-    for node in tree.all_nodes():
-        if node.is_leaf():
-            if not check_homogeneity(node.data.data):
-                image_data = node.data.data
-                hsplit = np.array_split(image_data, 2, 1)
-                for x in range(len(hsplit)):
-                    vsplit = np.array_split(hsplit[x], 2, 0)
-                    for y in range(len(vsplit)):
-                        quad = vsplit[y]
-                        if(quad.shape[0] > 1 and quad.shape[1] > 1):
-                            tree.create_node(parent=node, data=Quad(x,y, quad))
-                            tree = process_nodes(tree)
-            else:
-                return tree
-    
-    return tree
-            
-            
-            
-
-def segmentateImage(depth_image, depth_scale):
-    depth_image_3d = np.dstack((depth_image, depth_image, depth_image))
-    depth_image_3d.fill(0)
-    print(depth_image)
-    tree = Tree()
-    tree.create_node("0",0, data=Quad(0,0,depth_image))
-    tree = process_nodes(tree)
-    tree.show()
-
-    for node in tree.leaves():
-        print("%f %f" % (node.data.indexX, node.data.indexY))
-
-
-
-    return depth_image_3d
-
-def kmean(depth_colormap, color_image):
-    twoDiamge = depth_colormap.reshape((-1,3))
-    twoDiamge = np.float32(twoDiamge)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,10,1.0)
-    K = 3
-    attempts = 10
-    ret, label, center= cv2.kmeans(twoDiamge, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
-    center = np.uint8(center)
-    res = center[label.flatten()]
-    result_image = res.reshape((depth_colormap.shape))
-
-    low_white = np.array([80,101,110])
-    high_white = np.array([184, 180, 172])
-
-    #mask = cv2.inRange(color_image, low_white, low_white)
-    #mask = 255-mask
-    
-    for c in center:
-        mask = cv2.inRange(result_image, c, c)
-        contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)
-        print("Found %f " %len(contours))
-        if contours:
-            (x_min, y_min, box_width, box_height) = cv2.boundingRect(contours[0])
-            cv2.rectangle(color_image, (x_min - 15, y_min -15),
-                    (x_min + box_width + 15, y_min + box_height + 15),
-                        (0,int(c[0]),0), 4)
-
 
 def get_shelf(color, depth, corners, ids):
     mask = np.zeros(color.shape, np.uint8)
@@ -108,7 +23,15 @@ def get_shelf(color, depth, corners, ids):
 			# ArUco marker
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-            markerCenters[markerID] = [cX, cY]
+            if markerID == 0:
+                markerCenters[markerID] = [bottomRight[0], bottomRight[1]]
+            if markerID == 1:
+                markerCenters[markerID] = [bottomLeft[0], bottomLeft[1]]
+            if markerID == 2:
+                markerCenters[markerID] = [topRight[0], topRight[1]]
+            if markerID == 3:
+                markerCenters[markerID] = [topLeft[0], topLeft[1]]
+
             cv2.circle(color, (cX, cY), 4, (0, 0, 255), -1)
             
         if(len(markerCenters) >= 4):
@@ -121,22 +44,6 @@ def get_shelf(color, depth, corners, ids):
         
     return color, depth
 
-def findObjects(img, amount):
-    objects = []
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY)
-
-    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-
-    #cv2.drawContours(image=shelf_image, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-    if len(contours) > amount:
-        for box in range(1, amount+1):
-            (x_min, y_min, box_width, box_height) = cv2.boundingRect(contours[box])
-            objects.append([x_min, y_min, box_width, box_height])
-
-    
-    return objects
 
 def findDepthObjects(img, amount):
     objects = []
@@ -157,40 +64,41 @@ def findDepthObjects(img, amount):
         objects.append([x_min, y_min, box_width, box_height])
     return objects
 
-def checkORB(input, amount):
-    points = []
-    orb = cv2.ORB_create()
-    kp, des = orb.detectAndCompute(input, None)
-    pts = cv2.KeyPoint_convert(kp)
 
-    if(len(pts) > 10):
-        kmeans = KMeans(n_clusters=(amount+4), init='k-means++', max_iter=300, n_init=10, random_state=0)
-        pred_y = kmeans.fit_predict(pts)
-        points = kmeans.cluster_centers_
+def findContours(img, amount):
+    objects = []
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    contours, hierarchy = cv2.findContours(image=img, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    contours = contours[:amount]
+    toDraw = []
 
-    return points
+    return contours
 
+def maskShelf(color_img):
+    hsv = cv2.cvtColor(color_img, cv2.COLOR_BGR2HSV)
 
-def findBlob(input, color_image):
-    params = cv2.SimpleBlobDetector_Params()
+    mask_background = cv2.inRange(hsv,(15,35,100),(180,255,255))
 
-    params.filterByColor = True
-    params.blobColor = 0
+    kernel = np.ones((5,5),np.uint8)          
+    mask_background = cv2.morphologyEx(mask_background, cv2.MORPH_CLOSE, kernel)
 
-    params.filterByArea = True
-    params.minArea = 100
+    color_img = cv2.bitwise_and(color_img,color_img,mask =  mask_background)
 
-    detector = cv2.SimpleBlobDetector_create(params)
-    input_gray = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-    keypoints = detector.detect(input_gray)
-    print(keypoints)
-    color_image = cv2.drawKeypoints(color_image, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    return color_img
 
-    return color_image
+def getDepthOfRect(depth, rect):
+    x,y,w,h = rect
+    depth = depth[int(x+w/2),int(y+h/2)].astype(float)
+    depth = depth * depth_scale
+    return depth
 
+def getDepth(point, depth, aligned_depth_frame):
+    depth_intrinsic = aligned_depth_frame.profile.as_video_stream_profile().intrinsics
+    result = rs.rs2_deproject_pixel_to_point(depth_intrinsic, [point[0], point[1]], depth)
+    result_string = "X: %f, Y: %f, Z: %f" % (result[0], result[2], result[1]) 
 
-
-
+    return result_string
 
 
 pipeline = rs.pipeline()
@@ -232,33 +140,41 @@ try:
         depth_image = np.asanyarray(aligned_depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
+        depth_image = cv2.flip(depth_image, -1)
+        color_image = cv2.flip(color_image, -1)
+
         depth_image[depth_image > 3000] = 0
         #returnedImage = segmentateImage(depth_image, depth_scale)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_HSV)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.5), cv2.COLORMAP_HSV)
 
         (corners, ids, rejected) = cv2.aruco.detectMarkers(color_image, arucoDict, parameters=arucoParmas)
         shelf_image, shelf_image_depth = get_shelf(color_image, depth_image,corners, ids)
         
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(shelf_image_depth, alpha=0.1), cv2.COLORMAP_HSV)
 
-        objects_depth = findDepthObjects(shelf_image_depth, 2)
-        for ob in objects_depth:
-            cv2.rectangle(color_image, (ob[0] - 15, ob[1] -15),
-                (ob[0] + ob[2] + 15, ob[1] + ob[3] + 15),
-                (0,255,0), 4)
+        maskedImage = maskShelf(shelf_image)
 
-        objects = findObjects(shelf_image,2)
-        for ob in objects:
-            cv2.rectangle(color_image, (ob[0] - 15, ob[1] -15),
-                (ob[0] + ob[2] + 15, ob[1] + ob[3] + 15),
-                (0,0,255), 4)
-        
-        orb_points = checkORB(shelf_image, 2)
-        for point in orb_points:
-            cv2.circle(color_image,tuple(point),4,(255,0,0), -1)
+        contours = findContours(maskedImage,1)
+        cv2.drawContours(image=maskedImage, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
 
-        cv2.imshow('Aligned', color_image)
+        for c in contours:
+            rect = cv2.boundingRect(c)
+            x,y,w,h = rect
+            depthLevel = getDepthOfRect(depth_image,rect)
+            comx, comy = c.mean(axis=0)[0]
+
+            coords = getDepth([comx, comy], depthLevel, aligned_depth_frame)
+            cv2.rectangle(color_image, rect, (0,0,255),4)
+            cv2.putText(color_image, coords, (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            
+            cv2.circle(color_image, (int(comx),int(comy)), 4, (0,255,0), -1)
+            #cv2.circle(color_image, (int(x+w/2), int(y+h/2)), 4, (0, 255, 0), -1)
+
+            
+        com = np.hstack((color_image, maskedImage))
+        com = np.hstack((com, depth_colormap))
+        cv2.imshow('Aligned', com)
         key = cv2.waitKey(1)
         if key == 27:
             cv2.destroyAllWindows()
